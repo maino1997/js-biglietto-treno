975e3bfeb4cf59a0d5c1418bca091069a560a81a
Sasha Mainardi <sashamainardi4@gmail.com>
2021-12-21 17:15:22 +0100

    added spaces

--- script.js
+++ script.js
@@ -11,15 +11,21 @@
 const ticketDisplay = document.getElementById('ticketPrice');
 let finalPrice;
 
+
+
 // Richiste all'utente 
 const distance = parseInt( prompt('Quanti km vuoi percorrere?' , '50').trim());
 const age = parseInt(prompt('Quanti anni hai?' , '30').trim());
+
+
 
 
 // Validazione Input numerici utente 
 if ( isNaN(distance) || isNaN(age) ){
     alert('Valori inseriti non validi, reinserire!')
 }
+
+
 
 // calcolare prezzo senza sconto 
 const plainPrice = distance * 0.21;
@@ -38,9 +44,13 @@ if (age < 18 ){
 
 console.log(finalPrice);
 
+
+
 // rendere presentabile il prezzo 
 finalPrice = finalPrice.toFixed(2);
 console.log(`Il prezzo scontato è ${finalPrice}`);
+
+
 
 // Iniettare in pagina il risultato 
 ticketDisplay.innerText = `Il prezzo scontato è ${finalPrice} euro`;
