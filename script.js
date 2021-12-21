060ca6c5bc16839b2d6a0db84e1cdb7079679a1b
Sasha Mainardi <sashamainardi4@gmail.com>
2021-12-21 16:14:53 +0100

    questions to user and plainPrice

---
+++ script.js
@@ -0,0 +1,17 @@
+// Chiedere all'utente i km da percorrere 
+// chiedere all'utente l'età
+// calcolare prezzo senza sconto 
+// calcolare prezzo con sconto minorenni 
+// poi con sconto senior 
+// rendere presentabile il prezzo 
+
+
+// Richiste all'utente 
+
+const distance = parseInt('Quanti km vuoi percorrere?' , '50'));
+const age = parseInt(prompt('Quanti anni hai?' , '30'));
+
+// calcolare prezzo senza sconto 
+const plainPrice = distance * 0.21;
+console.log(plainPrice);
+
